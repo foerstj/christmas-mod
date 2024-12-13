9a8bd68e9ffb20a965b564139af1666a959898d2
Johannes Förstner <johannes@famfoerstner.de>
2024-12-13 22:07:40 +0100

    Oops, included .psd files in build

--- build-nomusic.bat
+++ build-nomusic.bat
@@ -9,7 +9,7 @@ set tc=%TankCreator%
 
 :: Compile main resource file without music
 rmdir /S /Q "%tmp%\Bits"
-robocopy "%bits%\art" "%tmp%\Bits\art" /E
+robocopy "%bits%\art" "%tmp%\Bits\art" /E /xf *.psd
 robocopy "%bits%\world\contentdb" "%tmp%\Bits\world\contentdb" /E
 robocopy "%bits%\world\global" "%tmp%\Bits\world\global" /E
 :: no music! :: robocopy "%bits%\sound" "%tmp%\Bits\sound" /E
