1100c02b36deb96d39aeab7c85cd24a3355cd894
Johannes Förstner <johannes@famfoerstner.de>
2024-12-13 21:50:09 +0100

    Cleanup repo scripts somewhat

--- cleanup.bat
+++ cleanup.bat
@@ -1,6 +1,3 @@
-:: This script is supposed to be executed from your DS installation folder.
-:: TankCreator is expected to be in a sibling dir.
-
 :: name of mod
 set mod=Christmas
 :: path of DS installation
--- run.bat
+++ run.bat
@@ -1,14 +1,7 @@
-:: This script is supposed to be executed from your DS installation folder.
-:: TankCreator is expected to be in a sibling dir.
-
-:: name of mod
-set mod=Christmas
 :: path of Bits dir
 set bits=%~dp0.
 :: path of DS installation
 set ds=%DungeonSiege%
-:: path of TankCreator
-set tc=%TankCreator%
 
 :: build
 call "%bits%\build.bat" %*
